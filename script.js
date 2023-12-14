03cd31a086bff91caa6453121975b70664e806ad
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-14 13:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // Check if either of the strings is empty
+  if (s1.length === 0 || s2.length === 0) {
+    return -1;
+  }
+
+  // Convert both strings to lowercase for case-insensitive comparison
+  const lowerS1 = s1.toLowerCase();
+  const lowerS2 = s2.toLowerCase();
+
+  // Find the index of the lowercase s2 in the lowercase s1
+  const index = lowerS1.indexOf(lowerS2);
+
+  return index;
 }
 
-// Please do not change the code below
+// Do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
